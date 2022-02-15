e004ab64dde2be48c96f5cc66b08688b80970316
Matti Airas <mairas@iki.fi>
2022-02-15 16:06:55 +0200

    Finish an MVP

--- src/main.cpp
+++ src/main.cpp
@@ -1,125 +1,62 @@
-// Signal K application template file.
-//
-// This application demonstrates core SensESP concepts in a very
-// concise manner. You can build and upload the application as is
-// and observe the value changes on the serial port monitor.
-//
-// You can use this source file as a basis for your own projects.
-// Remove the parts that are not relevant to you, and add your own code
-// for external hardware libraries.
 
-#include "sensesp/sensors/analog_input.h"
-#include "sensesp/sensors/digital_input.h"
+#include <Adafruit_HMC5883_U.h>
+
 #include "sensesp/sensors/sensor.h"
-#include "sensesp/signalk/signalk_output.h"
-#include "sensesp/system/lambda_consumer.h"
+#include "sensesp/transforms/angle_correction.h"
 #include "sensesp_app_builder.h"
+#include "sensesp_nmea0183/wiring.h"
 
 using namespace sensesp;
 
-reactesp::ReactESP app;
+ReactESP app;
 
-// The setup function performs one-time application initialization.
+constexpr int kGNSSBitRate = 9600;
+constexpr int kGNSSRxPin = 15;
+// set the Tx pin to -1 if you don't want to use it
+constexpr int kGNSSTxPin = 13;
+
+// SH-ESP32 default SDA and SCL pins
+constexpr int kSDAPin = 16;
+constexpr int kSCLPin = 17;
+
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+
 void setup() {
+  SensESPAppBuilder builder;
+  sensesp_app = builder.set_hostname("sensesp-bn880")
+                    ->enable_ota("thisisfine")
+                    ->get_app();
+
 #ifndef SERIAL_DEBUG_DISABLED
   SetupSerialDebug(115200);
 #endif
 
-  // Construct the global SensESPApp() object
-  SensESPAppBuilder builder;
-  sensesp_app = (&builder)
-                    // Set a custom hostname for the app.
-                    ->set_hostname("my-sensesp-project")
-                    // Optionally, hard-code the WiFi and Signal K server
-                    // settings. This is normally not needed.
-                    //->set_wifi("My WiFi SSID", "my_wifi_password")
-                    //->set_sk_server("192.168.10.3", 80)
-                    ->get_app();
+  // initialize the I2C bus
+  Wire.begin(kSDAPin, kSCLPin);
 
-  // GPIO number to use for the analog input
-  const uint8_t kAnalogInputPin = 36;
-  // Define how often (in milliseconds) new samples are acquired
-  const unsigned int kAnalogInputReadInterval = 500;
-  // Define the produced value at the maximum input voltage (3.3V).
-  // A value of 3.3 gives output equal to the input voltage.
-  const float kAnalogInputScale = 3.3;
+  HardwareSerial* serial = &Serial1;
+  serial->begin(kGNSSBitRate, SERIAL_8N1, kGNSSRxPin, kGNSSTxPin);
 
-  // Create a new Analog Input Sensor that reads an analog input pin
-  // periodically.
-  auto* analog_input = new AnalogInput(
-      kAnalogInputPin, kAnalogInputReadInterval, "", kAnalogInputScale);
+  NMEA0183Input* nmea_input = new NMEA0183Input(serial);
+  ConnectLocationSKOutputs(nmea_input);
 
-  // Add an observer that prints out the current value of the analog input
-  // every time it changes.
-  analog_input->attach([analog_input]() {
-    debugD("Analog input value: %f", analog_input->get());
+  debugD("Initializing HM5883...");
+  // Initialize the magnetometer. The call always returns true,
+  // so we can't use the return value to indicate hardware presence.
+  mag.begin();
+
+  auto heading_magnetic = new RepeatSensor<float>(1000, []() -> float {
+    sensors_event_t event;
+    mag.getEvent(&event);
+    float heading = atan2(event.magnetic.y, event.magnetic.x);
+    return heading;
   });
+  auto hdg_correction =
+      new AngleCorrection(0, 0, "/Compass/Heading Correction");
+  auto hdg_mag_output = new SKOutputFloat("navigation.headingMagnetic",
+                                          "/SK Path/Heading (Magnetic)");
+  heading_magnetic->connect_to(hdg_correction)->connect_to(hdg_mag_output);
 
-  // Set GPIO pin 15 to output and toggle it every 650 ms
-
-  const uint8_t kDigitalOutputPin = 15;
-  const unsigned int kDigitalOutputInterval = 650;
-  pinMode(kDigitalOutputPin, OUTPUT);
-  app.onRepeat(kDigitalOutputInterval, [kDigitalOutputPin]() {
-    digitalWrite(kDigitalOutputPin, !digitalRead(kDigitalOutputPin));
-  });
-
-  // Read GPIO 14 every time it changes
-
-  const uint8_t kDigitalInput1Pin = 14;
-  auto* digital_input1 =
-      new DigitalInputChange(kDigitalInput1Pin, INPUT_PULLUP, CHANGE);
-
-  // Connect the digital input to a lambda consumer that prints out the
-  // value every time it changes.
-
-  // Test this yourself by connecting pin 15 to pin 14 with a jumper wire and
-  // see if the value changes!
-
-  digital_input1->connect_to(new LambdaConsumer<bool>(
-      [](bool input) { debugD("Digital input value changed: %d", input); }));
-
-  // Create another digital input, this time with RepeatSensor. This approach
-  // can be used to connect external sensor library to SensESP!
-
-  const uint8_t kDigitalInput2Pin = 13;
-  const unsigned int kDigitalInput2Interval = 1000;
-
-  // Configure the pin. Replace this with your custom library initialization
-  // code!
-  pinMode(kDigitalInput2Pin, INPUT_PULLUP);
-
-  // Define a new RepeatSensor that reads the pin every 100 ms.
-  // Replace the lambda function internals with the input routine of your custom
-  // library.
-
-  // Again, test this yourself by connecting pin 15 to pin 13 with a jumper
-  // wire and see if the value changes!
-
-  auto* digital_input2 = new RepeatSensor<bool>(
-      kDigitalInput2Interval,
-      [kDigitalInput2Pin]() { return digitalRead(kDigitalInput2Pin); });
-
-  // Connect the analog input to Signal K output. This will publish the
-  // analog input value to the Signal K server every time it changes.
-  analog_input->connect_to(new SKOutputFloat(
-      "sensors.analog_input.voltage",         // Signal K path
-      "/sensors/analog_input/voltage",        // configuration path, used in the
-                                              // web UI and for storing the
-                                              // configuration
-      new SKMetadata("V",                     // Define output units
-                     "Analog input voltage")  // Value description
-      ));
-
-  // Connect digital input 2 to Signal K output.
-  digital_input2->connect_to(new SKOutputBool(
-      "sensors.digital_input2.value",          // Signal K path
-      "/sensors/digital_input2/value",         // configuration path
-      new SKMetadata("",                       // No units for boolean values
-                     "Digital input 2 value")  // Value description
-      ));
-
-  // Start networking, SK server connections and other SensESP internals
   sensesp_app->start();
 }
 
